4d8a7649c57950853676da9f12112d9bb278ed2e
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-08-26 13:40:50 -0700

    refactor: simplify assets; remove interface and make it a struct with private fields

--- pkg/mirror/mirror.go
+++ pkg/mirror/mirror.go
@@ -1,6 +1,7 @@
 package mirror
 
 import (
+	"errors"
 	"io"
 	"net/url"
 	"time"
@@ -11,23 +12,17 @@ import (
 
 // Configure common retry policies for mirrors.
 const (
+	filenameChecksums = "SHA512-SUMS.txt"
+
 	retryCount   = 3
 	retryWait    = time.Second
 	retryWaitMax = 10 * time.Second
 )
 
-/* -------------------------------------------------------------------------- */
-/*                              Interface: Asset                              */
-/* -------------------------------------------------------------------------- */
-
-// An interface representing a file/directory that can be downloaded from a
-// Godot project mirror.
-type Asset interface {
-	Name() string
-	URL() url.URL
-
-	Download(w io.Writer) error
-}
+var (
+	ErrInvalidSpecification = errors.New("invalid specification")
+	ErrInvalidURL           = errors.New("invalid URL")
+)
 
 /* -------------------------------------------------------------------------- */
 /*                              Interface: Mirror                             */
@@ -38,35 +33,36 @@ type Mirror interface {
 	Checksum(v godot.Version) (Asset, error)
 	Executable(ex godot.Executable) (Asset, error)
 
-	Has(ex godot.Executable) bool
+	Supports(v godot.Version) bool
 }
 
 /* -------------------------------------------------------------------------- */
-/*                                Struct: asset                               */
+/*                                Struct: Asset                               */
 /* -------------------------------------------------------------------------- */
 
-// The GitHub-specific implementation of a release asset.
-type asset struct {
+// A struct representing a file/directory that can be downloaded from a Godot
+// project mirror.
+type Asset struct {
 	client *resty.Client
 	name   string
-	url    url.URL
+	url    *url.URL
 }
 
 /* ------------------------------ Method: Name ------------------------------ */
 
-func (a *asset) Name() string {
+func (a Asset) Name() string {
 	return a.name
 }
 
 /* ------------------------------- Method: URL ------------------------------ */
 
-func (a *asset) URL() url.URL {
+func (a Asset) URL() *url.URL {
 	return a.url
 }
 
 /* ---------------------------- Method: Download ---------------------------- */
 
-func (a *asset) Download(w io.Writer) error {
+func (a Asset) Download(w io.Writer) error {
 	return nil
 }
 
