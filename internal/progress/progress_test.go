0328be1d66bb070def9f547491f3ca83f1222916
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-09-03 15:50:07 -0700

    feat: add Reset and Total methods to Progress, allowing post-init configuration

--- internal/progress/progress_test.go
+++ internal/progress/progress_test.go
@@ -19,11 +19,19 @@ func TestNew(t *testing.T) {
 		{size: 0, want: &Progress{}, err: ErrInvalidTotal},
 
 		// Valid inputs
-		{size: 10, want: &Progress{total: float64(10)}},
+		{size: 10},
 	}
 
 	for i, tc := range tests {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			var want *Progress
+
+			// Given: The correctly instantiated expected struct.
+			if tc.size > 0 {
+				want = &Progress{}
+				want.total.Store(tc.size)
+			}
+
 			// When: A new 'Progress' struct is created with the specified size.
 			got, err := New(tc.size)
 
@@ -32,8 +40,8 @@ func TestNew(t *testing.T) {
 				t.Fatalf("err: got %#v, want %#v", err, tc.err)
 
 			}
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Fatalf("output: got %#v, want %#v", got, tc.want)
+			if !reflect.DeepEqual(got, want) {
+				t.Fatalf("output: got %#v, want %#v", got, want)
 			}
 		})
 	}
@@ -56,7 +64,11 @@ func TestProgressPercentage(t *testing.T) {
 	for i, tc := range tests {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
 			// Given: A 'Progress' struct with the specified size.
-			p := Progress{total: float64(tc.size)}
+			p, err := New(tc.size)
+			if !errors.Is(err, nil) {
+				t.Fatalf("err: got %#v, want %#v", err, nil)
+
+			}
 
 			// Given: The specified progress is already made.
 			p.add(uint64(tc.current))
@@ -64,6 +76,90 @@ func TestProgressPercentage(t *testing.T) {
 			// When: The current progress percentage is collected.
 			// Then: It matches the expected value of 'current' / 'total'.
 			if got := p.Percentage(); got != tc.want {
+				t.Fatalf("output: got %#v, want %#v", got, tc.want)
+			}
+		})
+	}
+}
+
+/* -------------------------- Test: Progress.Reset -------------------------- */
+
+func TestProgressReset(t *testing.T) {
+	tests := []struct {
+		current, total uint64
+	}{
+		{current: 0, total: 1},
+		{current: 1, total: 0},
+		{current: 1, total: 1},
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%d-%d", tc.current, tc.total), func(t *testing.T) {
+			// Given: A 'Progress' struct with the specified total.
+			p := Progress{}
+			p.current.Store(tc.current)
+			p.total.Store(tc.total)
+
+			// Given: The specified progress is already made.
+			p.add(tc.current)
+
+			// When: The current progress is reset.
+			p.Reset()
+
+			// Then: The 'current' progress is reset to '0'.
+			if got := p.current.Load(); got != 0 {
+				t.Fatalf("output: got %#v, want %#v", got, 0)
+			}
+
+			// Then: The 'total' value is unchanged.
+			if got := p.total.Load(); got != tc.total {
+				t.Fatalf("output: got %#v, want %#v", got, tc.total)
+			}
+		})
+	}
+}
+
+/* -------------------------- Test: Progress.Total -------------------------- */
+
+func TestProgressTotal(t *testing.T) {
+	tests := []struct {
+		current, total, next, want uint64
+		err                        error
+	}{
+		// Invalid inputs
+		{current: 0, total: 1, next: 0, want: 1, err: ErrInvalidTotal},
+		{current: 1, total: 0, next: 0, want: 0, err: ErrInvalidTotal},
+
+		// Valid inputs
+		{current: 0, total: 1, next: 10, want: 10},
+		{current: 1, total: 0, next: 10, want: 10},
+		{current: 1, total: 1, next: 10, want: 10},
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%d-%d", tc.current, tc.total), func(t *testing.T) {
+			// Given: A 'Progress' struct with the specified total.
+			p := Progress{}
+			p.total.Store(tc.total)
+
+			// Given: The specified progress is already made.
+			p.add(tc.current)
+
+			// When: The 'total' is set to the new value.
+			err := p.Total(tc.next)
+
+			// Then: An error is returned if the new value is invalid.
+			if !errors.Is(err, tc.err) {
+				t.Fatalf("err: got %#v, want %#v", err, tc.err)
+			}
+
+			// Then: The 'current' progress is unchanged.
+			if got := p.current.Load(); got != tc.current {
+				t.Fatalf("output: got %#v, want %#v", got, tc.current)
+			}
+
+			// Then: The 'total' field is updated to the desired value.
+			if got := p.total.Load(); got != tc.want {
 				t.Fatalf("output: got %#v, want %#v", got, tc.want)
 			}
 		})
