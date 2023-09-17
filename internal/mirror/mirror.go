8ea9cab80aa4f7df719c2d63e86a661fa0fc41bb
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-09-17 16:38:11 -0700

    refactor: update mirror to use new artifacts

--- internal/mirror/mirror.go
+++ internal/mirror/mirror.go
@@ -3,8 +3,11 @@ package mirror
 import (
 	"errors"
 
+	"github.com/coffeebeats/gdenv/internal/godot/artifact"
+	"github.com/coffeebeats/gdenv/internal/godot/artifact/checksum"
+	"github.com/coffeebeats/gdenv/internal/godot/artifact/executable"
+	"github.com/coffeebeats/gdenv/internal/godot/artifact/source"
 	"github.com/coffeebeats/gdenv/internal/godot/version"
-	"github.com/coffeebeats/gdenv/pkg/godot"
 )
 
 var (
@@ -19,11 +22,14 @@ var (
 // An interface specifying methods for retrieving information about assets
 // available for download via a mirror host.
 type Mirror interface {
-	Checksum(v version.Version) (Asset, error)
-	Executable(ex godot.Executable) (Asset, error)
+	ExecutableArchive(ex executable.Executable) (artifact.Remote[executable.Archive], error)
+	ExecutableArchiveChecksums(v version.Version) (artifact.Remote[checksum.Executable], error)
+
+	SourceArchive(v version.Version) (artifact.Remote[source.Archive], error)
+	SourceArchiveChecksums(v version.Version) (artifact.Remote[checksum.Source], error)
 
 	// Issues a request to see if the mirror host has the specific version.
-	Has(v version.Version) bool
+	CheckIfSupports(v version.Version) bool
 
 	// Checks whether the version is broadly supported by the mirror. No network
 	// request is issued, but this does not guarantee the host has the version.
