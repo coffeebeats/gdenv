924aefa642a0409d0e47e16a86738993a8972bac
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-09-17 23:38:41 -0700

    feat: add source tests

---
+++ internal/godot/artifact/source/source_test.go
@@ -0,0 +1,30 @@
+package source
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/coffeebeats/gdenv/internal/godot/version"
+)
+
+/* ---------------------------- Test: Source.Name --------------------------- */
+
+func TestSourceName(t *testing.T) {
+	tests := []struct {
+		version version.Version
+		want    string
+	}{
+		// Valid inputs
+		{version: version.Version{}, want: "godot-0.0-stable"},
+		{version: version.Godot3(), want: "godot-3.0-stable"},
+		{version: version.MustParse("v4.1.1-dev1"), want: "godot-4.1.1-dev1"},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("%d-'%s'", i, tc.version), func(t *testing.T) {
+			if got := (Source{tc.version}).Name(); got != tc.want {
+				t.Fatalf("output: got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
