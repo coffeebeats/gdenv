fba56099fdf987c4dc652419ed101314163407a5
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-10-10 22:07:33 -0700

    simplify artifact package

--- internal/godot/artifact/artifact.go
+++ internal/godot/artifact/artifact.go
@@ -1,9 +1,15 @@
 package artifact
 
 import (
+	"errors"
+	"net/url"
+	"os"
+
 	"github.com/coffeebeats/gdenv/internal/godot/platform"
 	"github.com/coffeebeats/gdenv/internal/godot/version"
 )
+
+var ErrMissingPath = errors.New("missing path")
 
 /* -------------------------------------------------------------------------- */
 /*                             Interface: Artifact                            */
@@ -32,4 +38,39 @@ type Versioned interface {
 	Artifact
 
 	Version() version.Version
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                Struct: Local                               */
+/* -------------------------------------------------------------------------- */
+
+// A wrapper around an 'Artifact' which is locally-available on the file system.
+type Local[T Artifact] struct {
+	Artifact T
+	Path     string
+}
+
+/* ----------------------------- Method: Exists ----------------------------- */
+
+// Returns whether the downloaded file exists on the local file system.
+func (l Local[T]) Exists() (bool, error) {
+	if l.Path == "" {
+		return false, ErrMissingPath
+	}
+
+	if _, err := os.Stat(l.Path); err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               Struct: Remote                               */
+/* -------------------------------------------------------------------------- */
+
+// A wrapper around an 'Artifact' which is hosted on the internet.
+type Remote[T Artifact] struct {
+	Artifact T
+	URL      *url.URL
 }
