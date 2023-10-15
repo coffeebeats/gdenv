7cd79efe3b9f2acdcd21214f5a2064ac0edab786
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-10-15 15:56:46 -0700

    implement download functions which validate checksums

--- pkg/download/source.go
+++ pkg/download/source.go
@@ -9,10 +9,70 @@ import (
 	"github.com/coffeebeats/gdenv/internal/godot/artifact/source"
 	"github.com/coffeebeats/gdenv/internal/godot/mirror"
 	"github.com/coffeebeats/gdenv/internal/godot/version"
+	"golang.org/x/sync/errgroup"
 )
 
 type localSourceArchive = artifact.Local[source.Archive]
 type localSourceChecksums = artifact.Local[checksum.Source]
+
+/* -------------------------------------------------------------------------- */
+/*                   Function: SourceWithChecksumValidation                   */
+/* -------------------------------------------------------------------------- */
+
+func SourceWithChecksumValidation(
+	ctx context.Context,
+	m mirror.Mirror,
+	v version.Version,
+	out string,
+) (artifact.Local[source.Archive], error) {
+	chSource, chChecksums := make(chan artifact.Local[source.Archive]), make(chan artifact.Local[checksum.Source])
+	defer close(chSource)
+	defer close(chChecksums)
+
+	eg, ctxDownload := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		result, err := Source(ctxDownload, m, v, out)
+		if err != nil {
+			return err
+		}
+
+		select {
+		case chSource <- result:
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+
+		return nil
+	})
+
+	eg.Go(func() error {
+		result, err := SourceChecksums(ctxDownload, m, v, out)
+		if err != nil {
+			return err
+		}
+
+		select {
+		case chChecksums <- result:
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+
+		return nil
+	})
+
+	sourceArchive, checksums := <-chSource, <-chChecksums
+
+	if err := eg.Wait(); err != nil {
+		return localSourceArchive{}, err
+	}
+
+	if err := checksum.Compare[source.Archive](ctx, sourceArchive, checksums); err != nil {
+		return localSourceArchive{}, err
+	}
+
+	return sourceArchive, nil
+}
 
 /* -------------------------------------------------------------------------- */
 /*                              Function: Source                              */
@@ -35,13 +95,14 @@ func Source(
 		return localSourceArchive{}, err
 	}
 
+	out = filepath.Join(out, remote.Artifact.Name())
 	if err := m.Client().DownloadTo(ctx, remote.URL, out); err != nil {
 		return localSourceArchive{}, err
 	}
 
 	return localSourceArchive{
 		Artifact: remote.Artifact,
-		Path:     filepath.Join(out, remote.Artifact.Name()),
+		Path:     out,
 	}, nil
 }
 
@@ -66,12 +127,13 @@ func SourceChecksums(
 		return localSourceChecksums{}, err
 	}
 
+	out = filepath.Join(out, remote.Artifact.Name())
 	if err := m.Client().DownloadTo(ctx, remote.URL, out); err != nil {
 		return localSourceChecksums{}, err
 	}
 
 	return localSourceChecksums{
 		Artifact: remote.Artifact,
-		Path:     filepath.Join(out, remote.Artifact.Name()),
+		Path:     out,
 	}, nil
 }
