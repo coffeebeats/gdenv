b8b07cd072f35a851ba83ced0a9c5b67dfbbfc7e
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-10-22 21:57:28 -0700

    fix: add windows support to shim

--- cmd/gdenv-shim/main.go
+++ cmd/gdenv-shim/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
+	"os/exec"
 	"syscall"
 
 	"github.com/coffeebeats/gdenv/internal/godot/platform"
 	"github.com/coffeebeats/gdenv/pkg/install"
+	"github.com/coffeebeats/gdenv/pkg/pin"
 	"github.com/coffeebeats/gdenv/pkg/store"
+)
+
+var (
+	ErrMissingPin   = errors.New("no version selected; try setting a version pin with 'gdenv pin'")
+	ErrNotInstalled = errors.New("pinned version not installed; try installing with 'gdenv install'")
 )
 
 func main() {
@@ -53,12 +61,25 @@ func execute(ctx context.Context) error {
 
 	binary, err := install.Which(ctx, storePath, p, wd)
 	if err != nil {
+		if errors.Is(err, pin.ErrMissingPin) {
+			return ErrMissingPin
+		}
+
+		if errors.Is(err, install.ErrNotInstalled) {
+			return ErrNotInstalled
+		}
+
 		return err
 	}
 
-	return syscall.Exec(
-		binary,
-		append([]string{binary}, os.Args[1:]...),
-		os.Environ(),
-	)
+	switch p.OS {
+	case platform.Windows:
+		return exec.Command(binary, os.Args[1:]...).Run()
+	default:
+		return syscall.Exec(
+			binary,
+			os.Args[1:],
+			os.Environ(),
+		)
+	}
 }
