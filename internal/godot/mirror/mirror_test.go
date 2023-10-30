d322b75b985c0eae584768b2e1595e771e5f971a
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-10-29 18:10:36 -0700

    feat: add missing tests for generating source code artifacts from mirrors

--- internal/godot/mirror/mirror_test.go
+++ internal/godot/mirror/mirror_test.go
@@ -3,7 +3,23 @@ package mirror
 import (
 	"net/url"
 	"testing"
+
+	"github.com/coffeebeats/gdenv/internal/godot/artifact/checksum"
+	"github.com/coffeebeats/gdenv/internal/godot/version"
 )
+
+/* ----------------------- Function: mustMakeNewSource ---------------------- */
+
+func mustMakeNewSource(t *testing.T, v version.Version) checksum.Source {
+	t.Helper()
+
+	s, err := checksum.NewSource(v)
+	if err != nil {
+		t.Fatalf("test setup: %#v", err)
+	}
+
+	return s
+}
 
 /* ------------------------- Function: mustParseURL ------------------------- */
 
