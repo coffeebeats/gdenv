a70132cb51a2e9ef622ed24a667f7194415349dd
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-11-04 18:02:39 -0700

    chore: release v0.6.1 (#131)

--- cmd/gdenv/main.go
+++ cmd/gdenv/main.go
@@ -32,7 +32,7 @@ func main() { //nolint:funlen
 
 	app := &cli.App{
 		Name:    "gdenv",
-		Version: "v0.6.0", // x-release-please-version
+		Version: "v0.6.1", // x-release-please-version
 
 		Suggest:                true,
 		UseShortOptionHandling: true,
