f1efede7f062b76a38cf2aabc4b94e16cca79b6c
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-11-07 10:50:21 -0800

    chore: test source file change

--- pkg/store/path.go
+++ pkg/store/path.go
@@ -34,6 +34,10 @@ func Executable(storePath string, ex executable.Executable) (string, error) {
 		return "", ErrMissingStore
 	}
 
+	if storePath == "" {
+		return "", ErrMissingStore
+	}
+
 	pathExecutableDir, err := executableDir(storePath, ex)
 	if err != nil {
 		return "", err
