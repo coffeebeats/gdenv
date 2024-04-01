2d4d22d906cb59481f4c04b30271c144101af4de
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-03-31 20:04:11 -0700

    fix(store): use a file copy in `store.Add` to support different device volumes

--- internal/osutil/file.go
+++ internal/osutil/file.go
@@ -1,11 +1,51 @@
 package osutil
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
+
+	"github.com/coffeebeats/gdenv/internal/ioutil"
 )
+
+// Only write to 'out'; create a new file/overwrite an existing.
+const copyFileWriteFlag = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+
+/* -------------------------------------------------------------------------- */
+/*                             Function: CopyFile                             */
+/* -------------------------------------------------------------------------- */
+
+// CopyFile is a utility function for copying an 'io.Reader' to a new file
+// created with the specified 'os.FileMode'.
+func CopyFile(ctx context.Context, src, out string) error {
+	f, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		return err
+	}
+
+	dst, err := os.OpenFile(out, copyFileWriteFlag, info.Mode())
+	if err != nil {
+		return err
+	}
+
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, ioutil.NewReaderWithContext(ctx, f.Read)); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 /* -------------------------------------------------------------------------- */
 /*                             Function: EnsureDir                            */
